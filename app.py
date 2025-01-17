4774125cbdfe8d1fb19efb3ab132426a51c9e873
kahee <bona_323@naver.com>
2025-01-17 11:55:16 +0900

    2nd commit

--- app.py
+++ app.py
@@ -1,5 +1,9 @@
 import streamlit as st
 
 st.title('Deploy App')
-
 st.write('My First Deploy App!')
+
+
+import os
+key = os.environ.get('MY_SECRET', 'NOT SET YET')
+st.write(f'Server Key : {key}')
